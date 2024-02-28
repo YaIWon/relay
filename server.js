f31478b8b44bf467100aeb01504fdfb156ac9ec6
YaIWon <158149598+YaIWon@users.noreply.github.com>
2024-02-27 23:40:11 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Set up a proxy route
+app.use(
+  '/', // Change this route to match the desired endpoint
+  createProxyMiddleware({
+    target: 'https://mainnet.infura.io/v3/487e87a62b4543529a6fd0bbaef2020f',
+    changeOrigin: true,
+    secure: true,
+  })
+);
+
+// Start the server
+const port = 8545; // Use the desired port, in this case, it's 8545
+app.listen(port, '127.0.0.1', () => {
+  console.log(`Server is running on http://127.0.0.1:${port}`);
+});
